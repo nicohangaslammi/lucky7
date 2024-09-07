42919c510ffb2586a29dfd36c6c5b581258978b8
Nico Hangaslammi <nico.hangaslammi@gmail.com>
2024-09-07 20:42:23 +0300

    add currency and replay

--- src/App.java
+++ src/App.java
@@ -4,22 +4,34 @@ public class App {
 	public static void main(String[] args) throws Exception {
 		Random random = new Random();
 
+		// Prices for amounts of seven. 1 = 3, 2 = 5 etc.
+		int[] winPrices = { 0, 3, 5, 7 };
 		int[] rolls = new int[3];
-		boolean win = false;
+		int winCount;
+		int currency = 5;
 
-		for (int i = 0; i < rolls.length; i++) {
-			int value = random.nextInt(10);
-			rolls[i] = value;
-			if (value == 7)
-				win = true;
-		}
+		while (currency > 0) {
+			currency--;
+			winCount = 0;
 
-		System.out.printf("\no-- Lucky 7 --o\n| [%d] [%d] [%d] |\no-------------o\n", rolls[0], rolls[1], rolls[2]);
+			// Roll numbers
+			for (int i = 0; i < rolls.length; i++) {
+				int value = random.nextInt(10);
+				rolls[i] = value;
+				if (value == 7)
+					winCount++;
+			}
 
-		if (win) {
-			System.out.printf("|   Voitit!   |\no-------------o\n");
-		} else {
-			System.out.printf("|   Hävisit!  |\no-------------o\n");
+			// Add price to currency
+			currency += winPrices[winCount];
+
+			System.out.printf("\no-- Lucky 7 --o\n| [%d] [%d] [%d] |\no-------------o\n", rolls[0], rolls[1], rolls[2]);
+
+			if (winCount > 0) {
+				System.out.printf("|  Voitit %d$  |\no-------------o\n", winPrices[winCount]);
+			} else {
+				System.out.printf("|   Hävisit!  |\no-------------o\n");
+			}
 		}
 	}
 }
