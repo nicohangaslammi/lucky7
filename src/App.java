a53fd4a75c2a933551ef7c70cb1da7320872456d
Nico Hangaslammi <nico.hangaslammi@gmail.com>
2024-09-07 20:33:41 +0300

    add basic gameplay
    rolls 3 numbers, prints them and tells user if they've won

--- src/App.java
+++ src/App.java
@@ -1,5 +1,25 @@
+import java.util.Random;
+
 public class App {
 	public static void main(String[] args) throws Exception {
-		System.out.println("Hello, World!");
+		Random random = new Random();
+
+		int[] rolls = new int[3];
+		boolean win = false;
+
+		for (int i = 0; i < rolls.length; i++) {
+			int value = random.nextInt(10);
+			rolls[i] = value;
+			if (value == 7)
+				win = true;
+		}
+
+		System.out.printf("\no-- Lucky 7 --o\n| [%d] [%d] [%d] |\no-------------o\n", rolls[0], rolls[1], rolls[2]);
+
+		if (win) {
+			System.out.printf("|   Voitit!   |\no-------------o\n");
+		} else {
+			System.out.printf("|   Hävisit!  |\no-------------o\n");
+		}
 	}
 }
