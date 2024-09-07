5322613cc1a84d65a1c04a0925f2d53fe8493c2b
Nico Hangaslammi <nico.hangaslammi@gmail.com>
2024-09-07 21:07:21 +0300

    add currency selection and option to stop. remove print styling

--- src/App.java
+++ src/App.java
@@ -1,16 +1,24 @@
 import java.util.Random;
+import java.util.Scanner;
 
 public class App {
 	public static void main(String[] args) throws Exception {
 		Random random = new Random();
+		Scanner in = new Scanner(System.in);
 
 		// Prices for amounts of seven. 1 = 3, 2 = 5 etc.
 		int[] winPrices = { 0, 3, 5, 7 };
 		int[] rolls = new int[3];
 		int winCount;
-		int currency = 5;
+		int currency;
+		boolean wantToContinue = true;
+		String answer;
 
-		while (currency > 0) {
+		System.out.printf("\nLucky 7\nSyötä rahamäärä: ");
+		currency = in.nextInt();
+		in.nextLine(); // clear line break from Scanner
+
+		while (currency > 0 && wantToContinue) {
 			currency--;
 			winCount = 0;
 
@@ -25,13 +33,21 @@ public class App {
 			// Add price to currency
 			currency += winPrices[winCount];
 
-			System.out.printf("\no-- Lucky 7 --o\n| [%d] [%d] [%d] |\no-------------o\n", rolls[0], rolls[1], rolls[2]);
+			System.out.printf("\n[%d] [%d] [%d]", rolls[0], rolls[1], rolls[2]);
 
 			if (winCount > 0) {
-				System.out.printf("|  Voitit %d$  |\no-------------o\n", winPrices[winCount]);
-			} else {
-				System.out.printf("|   Hävisit!  |\no-------------o\n");
+				System.out.printf(" Voitit %d$", winPrices[winCount]);
+			}
+
+			System.out.printf("\nRahaa jäljellä: %d$\n", currency);
+
+			if (currency > 0) {
+				System.out.printf("Paina Enter jatkaaksesi pelaamista ");
+				answer = in.nextLine();
+				wantToContinue = answer.equals("");
 			}
 		}
+
+		System.out.printf("Kiitos pelaamisesta!\n");
 	}
 }
