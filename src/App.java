dee3de01c39509ba8b8b81b0cd35491ffbcab4bf
Nico Hangaslammi <nico.hangaslammi@gmail.com>
2024-09-09 11:44:14 +0300

    rename variables, edit and add comments

--- src/App.java
+++ src/App.java
@@ -8,45 +8,47 @@ public class App {
 
 		// Prices for amounts of seven. 1 = 3, 2 = 5 etc.
 		int[] winPrices = { 0, 3, 4, 6 };
+		
 		int[] rolls = new int[3];
-		int winCount = 0;
+		int sevenCount = 0;
 		int currency = 0;
 		boolean wantToContinue = true;
 		String userAnswer;
 
-		// Get currency amount from user
+		// Get starting currency amount from user
 		System.out.printf("\nLucky 7\nSyötä rahamäärä: ");
 		try {
 			currency = in.nextInt();
-			in.nextLine(); // clear line break from Scanner
+			in.nextLine(); // Clear line break from Scanner
 		} catch (Exception e) {
-			System.out.println("Määrä ei ole kelvollinen. Poistutaan\n");
+			System.out.printf("Määrä ei ole kelvollinen. Poistutaan.\n");
+			in.close();
 			return;
 		}
 
 		while (currency > 0 && wantToContinue) {
 			currency--;
-			winCount = 0;
+			sevenCount = 0;
 
 			// Roll numbers
 			for (int i = 0; i < rolls.length; i++) {
 				int value = random.nextInt(10);
 				rolls[i] = value;
 				if (value == 7)
-					winCount++;
+					sevenCount++;
 			}
 
 			// Add price to currency
-			currency += winPrices[winCount];
+			currency += winPrices[sevenCount];
 
+			// Print rolled numbers, did player win & remaining currency
 			System.out.printf("\n[%d] [%d] [%d]", rolls[0], rolls[1], rolls[2]);
-
-			if (winCount > 0) {
-				System.out.printf(" Voitit %d$", winPrices[winCount]);
+			if (sevenCount > 0) {
+				System.out.printf(" Voitit %d$", winPrices[sevenCount]);
 			}
-
 			System.out.printf("\nRahaa jäljellä: %d$\n", currency);
 
+			// Ask if user wants to continue playing
 			if (currency > 0) {
 				System.out.printf("Paina Enter jatkaaksesi pelaamista ");
 				userAnswer = in.nextLine();
@@ -55,5 +57,6 @@ public class App {
 		}
 
 		System.out.printf("Kiitos pelaamisesta!\n");
+		in.close();
 	}
 }
